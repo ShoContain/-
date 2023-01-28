120d3503b4881a01caeb9dbf332b5686e0ab19de
ShoContain <perblue.666@gmail.com>
2023-01-28 11:21:38 +0900

    add: lefletの導入

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import 'leaflet/dist/leaflet.css';
 
 import {
   getFirestore,
@@ -13,6 +14,7 @@ import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 const app = firebase.initializeApp(firebaseConfig);
 const db = getFirestore(app);
@@ -54,11 +56,17 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>My App</h1>
-      <p>Please sign-in:</p>
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-    </div>
+    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={[51.505, -0.09]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+    </MapContainer>
   );
 }
 
