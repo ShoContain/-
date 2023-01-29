c82fd8304a1a93d873ccf2b7d74ee5cc35135d55
ShoContain <perblue.666@gmail.com>
2023-01-29 21:22:50 +0900

    feat: 検索フォームをtailwindで実装

--- public/src/App.tsx
+++ public/src/App.tsx
@@ -1,72 +1,76 @@
-import { useState } from "react";
+import { FormEvent, useCallback, useState } from "react";
 import "./App.css";
-import 'leaflet/dist/leaflet.css';
+import "leaflet/dist/leaflet.css";
 
-import {
-  getFirestore,
-  connectFirestoreEmulator,
-  addDoc,
-  collection,
-  getDocs,
-} from "firebase/firestore";
-import { firebaseConfig } from "./credential";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
-import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-
-const app = firebase.initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth();
-
-connectFirestoreEmulator(db, "localhost", 8080);
-connectAuthEmulator(auth, "http://localhost:9099");
-
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: "popup",
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "http://localhost:5173/",
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-    firebase.auth.EmailAuthProvider.PROVIDER_ID,
-  ],
-};
-
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815,
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
-
-const querySnapshot = await getDocs(collection(db, "users"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${doc.data()}`);
-});
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import axios from "axios";
+import { LatLngExpression } from "leaflet";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [searchText, setSearchText] = useState("名古屋城");
+  const [center, setCenter] = useState({
+    lat: 35.1816051,
+    lng: 136.9054953,
+  });
+
+  const doSearch = useCallback(async (e: FormEvent) => {
+    e.preventDefault()
+    const res = await axios.get("https://nominatim.openstreetmap.org/search", {
+      params: {
+        q: searchText,
+        format: "json",
+        "accept-language": "ja",
+        limit: 1,
+      },
+    });
+    if (res.data.length > 0) {
+      const center = res.data[0];
+
+      setCenter({ lat: center.lat, lng: center.lon });
+    }
+  }, [searchText, setSearchText]);
+
+  const ChangeMapView = ({ center }: { center: LatLngExpression }) => {
+    const map = useMap();
+    map.setView(center, map.getZoom());
+    return null;
+  };
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="container mx-auto">
+      <div className="flex my-5">
+        <form onSubmit={doSearch}>
+          <input
+            type="text"
+            className="inline-flex items-center p-2.5 text-sm border border-r-2.5  bg-gray-200 text-gray-900"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            type="submit"
+            className="rounded-none rounded-r-lg p-2.5  bg-gray-50"
+          >
+            Search
+          </button>
+        </form>
+      </div>
+      <MapContainer center={center} zoom={16} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[center.lat, center.lng]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <ChangeMapView center={center} />
+      </MapContainer>
+    </div>
   );
 }
 
